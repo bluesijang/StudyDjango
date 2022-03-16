4ad27314ebae962db2cdca6febc9047d6767eb2a
bluesijang <101042649+bluesijang@users.noreply.github.com>
2022-03-17 00:10:13 +0900

    Update settings.py

--- DjangoReact/settings.py
+++ DjangoReact/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*nt0i0$h$41wc68)&%=6mhbi2j+ujbr=hl0+54mkg31@zqb9c('
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
